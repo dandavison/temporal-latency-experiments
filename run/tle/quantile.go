e9311c86ad842fd23c5d03ede333306ece14dd86
Dan Davison <dan.davison@temporal.io>
2024-09-11 13:33:41 -0400

    Fix sorting bug

--- run/tle/quantile.go
+++ run/tle/quantile.go
@@ -8,6 +8,7 @@ func Quantile(xs []int64, q float64) int64 {
 	if !(0 <= q && q < 1) {
 		panic("q must be in [0, 1)")
 	}
+	xs = append([]int64{}, xs...)
 	sort.Slice(xs, func(i, j int) bool {
 		return xs[i] < xs[j]
 	})
